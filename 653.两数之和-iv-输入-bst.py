cba340c8bc963a46e114bf44ded754929d4c035a
chenzilong <chenzilong_1227@foxmail.com>
2020-09-04 16:14:47 +0800

    u

--- 653.两数之和-iv-输入-bst.py
+++ 653.两数之和-iv-输入-bst.py
@@ -19,6 +19,25 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
-        
+        self.res = []
+        def preorder(node):
+            if not node:
+                return 
+            preorder(node.left)
+            self.res.append(node.val)
+            preorder(node.right)
+        preorder(root)
+        l, r = 0, len(self.res) - 1
+        while l < r:
+            if self.res[l] + self.res[r] == k:
+                return True
+            elif self.res[l] + self.res[r] < k:
+                l += 1
+            elif self.res[l] + self.res[r] > k:
+                r -= 1
+        return False
+
+
+
 # @lc code=end
 
